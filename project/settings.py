bd652210f71babf91365ff965d11cca01bd4e9fb
Aleksandr Semenov <semenlinux@gmail.com>
2021-03-25 14:01:45 +0300

    Load settings and credentials from env

---
+++ project/settings.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+    }
+}
+INSTALLED_APPS = ["datacenter"]
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+DEBUG = os.getenv("DJANGO_DEBUG")
+
+ROOT_URLCONF = "project.urls"
+
+ALLOWED_HOSTS = ["*"]
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+    },
+]
+
+
+USE_L10N = True
+
+LANGUAGE_CODE = "ru-ru"
+
+TIME_ZONE = "Europe/Moscow"
+
+USE_TZ = True
